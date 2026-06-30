d79a2d496a07eaf3a988b235ff3d1be075ed7b17
Avanindra Bose <avanindrabose0218@gmail.com>
2026-07-01 00:23:32 +0530

    Changed Integration Test to accomdate latest changes in Endpoints

--- tests/api/integration/test_recommend_flow.py
+++ tests/api/integration/test_recommend_flow.py
@@ -1,4 +1,31 @@
+async def login_test_user(client):
+    signup_response = await client.post(
+        "/auth/signup",
+        data={
+            "username": "Integration User",
+            "email": "integration@example.com",
+            "password": "StrongPass1!",
+        },
+        follow_redirects=False,
+    )
+    assert signup_response.status_code == 303
+    assert signup_response.headers["location"] == "/auth/login?signup=success"
+
+    login_response = await client.post(
+        "/auth/login",
+        data={
+            "email": "integration@example.com",
+            "password": "StrongPass1!",
+        },
+        follow_redirects=False,
+    )
+    assert login_response.status_code == 303
+    assert login_response.headers["location"] == "/dashboard"
+
+
 async def test_recommendation_search_flow(integration_async_client):
+    await login_test_user(integration_async_client)
+
     response = await integration_async_client.get(
         "/api/song/search",
         params={"song_name": "Blinding Lights", "artist_name": "The Weeknd"},
